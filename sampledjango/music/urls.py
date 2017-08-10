fa4636a2b9318cfa3be521dc7e3d73193e333040
Young Eun Kim <aakim0907@gmail.com>
2017-08-09 19:06:59 -0700

    Add detail page

--- sampledjango/music/urls.py
+++ sampledjango/music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    # /music/
+    url(r'^$', views.index, name='index'),
+
+    # /music/id/
+    url(r'^(?P<album_id>[0-9]+)$', views.detail, name='detail'),
 ]
--- sampledjango/music/views.py
+++ sampledjango/music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is music app homepage</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Deatils for Album id: " + str(album_id) + "</h2>")
