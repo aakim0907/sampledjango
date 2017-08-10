aa52feebff5a41722dfe7b5f621a8c2a00f59a54
Young Eun Kim <aakim0907@gmail.com>
2017-08-09 21:03:29 -0700

    create navbar

--- sampledjango/music/views.py
+++ sampledjango/music/views.py
@@ -1,26 +1,28 @@
-from django.http import Http404
-# from django.http import HttpResponse
-# from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Album
 
-# def index(request):
-#     all_albums = Album.objects.all()
-#     template = loader.get_template('music/index.html')
-#     context = {
-#         'all_albums': all_albums,
-#     }
-#     return HttpResponse(template.render(context, request))
+
 def index(request):
     all_albums = Album.objects.all()
     return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    # return HttpResponse("<h2>Deatils for Album id: " + str(album_id) + "</h2>")
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': 'You did not select a valid song',
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
